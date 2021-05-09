8152f1fc2d148d485ffe0127a7dbe0778d0deaa3
2Coolge <83459083+2Coolge@users.noreply.github.com>
2021-05-09 16:55:29 +0500

    Add files via upload

---
+++ fake_face.py
@@ -0,0 +1,151 @@
+from __future__ import division, print_function
+
+import os
+
+
+#mfor model
+import numpy as np
+
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+
+# Keras
+
+from keras.models import load_model
+#from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = './models/model_resnet.h5'
+
+image_dimensions = {'height':256, 'width':256, 'channels':3}
+
+class Classifier:
+    def __init__(self):
+        self.model = 0
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def fit(self, x, y):
+        return self.model.train_on_batch(x, y)
+
+    def get_accuracy(self, x, y):
+        return self.model.test_on_batch(x, y)
+
+    def load(self, path):
+        self.model.load_weights(path)
+
+
+class Meso4(Classifier):
+    def __init__(self, learning_rate=0.001):
+        self.model = self.init_model()
+        optimizer = Adam(lr=learning_rate)
+        self.model.compile(optimizer=optimizer,
+                           loss='mean_squared_error',
+                           metrics=['accuracy'])
+
+    def init_model(self):
+        x = Input(shape=(image_dimensions['height'],
+                         image_dimensions['width'],
+                         image_dimensions['channels']))
+
+        x1 = Conv2D(8, (3, 3), padding='same', activation='relu')(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
+
+        x2 = Conv2D(8, (5, 5), padding='same', activation='relu')(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
+
+        x3 = Conv2D(16, (5, 5), padding='same', activation='relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
+
+        x4 = Conv2D(16, (5, 5), padding='same', activation='relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation='sigmoid')(y)
+
+        return Model(inputs=x, outputs=y)
+# Instantiate a MesoNet model with pretrained weights
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(256, 256))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    #x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    result = np.round(preds, 1)
+    str = ""
+    if result == 0:
+        str = "Deep Fake Face"
+    else:
+        str = "Real Face"
+
+
+
+
+    return str
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        meso = Meso4()
+        meso.load('weights/Meso4_DF')
+        preds = model_predict(file_path, meso)
+        print(preds)
+        os.remove(file_path)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        #result = str(pred_class[0][0][1])               # Convert to string
+        return preds
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
